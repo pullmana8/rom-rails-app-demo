64cf58311e593b6b49ffe8a37fd68a3539bfd207
Antonette Caldwell <antonettecalwell@tutanota.com>
2019-05-11 11:55:58 -0500

    Complete migrations create profile

--- db/migrate/20190511164553_create-profiles.rb
+++ db/migrate/20190511164553_create-profiles.rb
@@ -1,4 +1,15 @@
 ROM::SQL.migration do
   change do
+    create_table :profiles do
+      primary_key :id
+
+      column :email, String, null: false, unique: true, index: true
+      column :display_name, String
+
+      column :private_email, TrueClass, default: true
+
+      column :bio, :text
+      column :state, String, default: 'active', index: true
+    end
   end
 end
