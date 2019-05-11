01a0b31a75b3d264742f169a4f6dcd3b08ad4326
Antonette Caldwell <antonettecalwell@tutanota.com>
2019-05-11 11:49:49 -0500

    Create Migrations

--- config/initializers/rom.rb
+++ config/initializers/rom.rb
@@ -1,3 +1,4 @@
 ROM::Rails::Railtie.configure do |config|
-  # config.gateways[:default] = [:sql, ENV.fetch('DATABASE_URL')]
+  config.gateways[:default] = [:sql,
+    ENV.fetch('DATABASE_URL')]
 end
